42de58f4a4e485ec756a90804d1cd7e12b177e62
Carl Cronsioe <carl.cronsioe@kambi.com>
2023-07-06 21:11:37 +0200

    asd

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -6,26 +6,16 @@ import Link from "next/link";
 import Navbar from "../components/Navbar";
 
 const Home: NextPage = () => {
-  const answerSheetUpdate = trpc.answerSheet.updatePoints.useMutation();
-
-  const action = () => {
-    answerSheetUpdate.mutate({
-      calculateAllPoints: true,
-    });
-  };
-
   return (
     <>
       <Head>
         <title>Tipset</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="text-black bg-white flex h-screen flex-col items-center">
-        <Navbar></Navbar>
+      <main className="text-black flex h-screen flex-col bg-snowStorm1">
+        <Navbar />
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-8">
           <AuthShowcase />
-
-          <button onClick={action}>Create!</button>
         </div>
       </main>
     </>
@@ -36,24 +26,11 @@ export default Home;
 
 const AuthShowcase: React.FC = () => {
   const { isSignedIn } = useAuth();
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery(
-    undefined,
-    { enabled: !!isSignedIn },
-  );
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       {isSignedIn && (
         <>
-          <p className="text-white text-center text-2xl">
-            {secretMessage && (
-              <span>
-                {" "}
-                {secretMessage} click the user button!
-                <br />
-              </span>
-            )}
-          </p>
           <div className="flex items-center justify-center">
             <UserButton
               appearance={{
--- apps/nextjs/tailwind.config.cjs
+++ apps/nextjs/tailwind.config.cjs
@@ -1,4 +1,37 @@
 /** @type {import("tailwindcss").Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   presets: [require("@acme/tailwind-config")],
+  theme: {
+    colors: {
+      slate: colors.white,
+      polarNight: "#4C566A",
+      snowStorm1: "#D8DEE9",
+      snowStorm2: "#E5E9F0",
+      snowStorm3: "#ECEFF4",
+      frost1: "#8FBCBB",
+      frost2: "#88C0D0",
+      frost3: "#81A1C1",
+      frost4: "#5E81AC",
+      auroraRed: "#BF616A",
+      auroraGreed: "#A3BE8C",
+      red: colors.red,
+      blue: colors.blue,
+      green: colors.green,
+      gray: colors.gray,
+    },
+    extend: {
+      screens: {
+        xs: "430px",
+        ...defaultTheme.screens,
+      },
+      animation: {
+        "spin-slow": "spin 4s linear infinite",
+      },
+    },
+  },
+  plugins: [],
 };
