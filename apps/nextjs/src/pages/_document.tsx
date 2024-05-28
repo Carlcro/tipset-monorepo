bf6d9465e11e9df6388a0071c0daf60856616d4e
Carl Cronsioe <carl.cronsioe@kambi.com>
2024-05-28 09:04:40 +0200

    analytics

--- apps/nextjs/src/pages/_document.tsx
+++ apps/nextjs/src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Document() {
   return (
@@ -124,6 +125,7 @@ export default function Document() {
       <body className="bg-frost2 text-polarNight">
         <Main />
         <NextScript />
+        <Analytics />
       </body>
     </Html>
   );
